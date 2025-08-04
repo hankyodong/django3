a3b16246daca2f3f1631f06d8e0fe32e1cd138ea
hankyodong <jsmhp01@gmail.com>
2025-08-03 18:00:14 -0700

    7일차

--- todolist/todo/forms.py
+++ todolist/todo/forms.py
@@ -1,16 +1,31 @@
 # todo / forms.py
 
-from django import forms
-from todo.models import Todo
+from django_summernote.widgets import SummernoteWidget  # 추가
 
+...
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ["title", "description", "start_date", "end_date"]
+        fields = ['title', 'description', 'start_date', 'end_date']
+        widgets = {
+            'description': SummernoteWidget(),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '제목을 입력해주세요.'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+        }
 
 
 class TodoUpdateForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ["title", "description", "start_date", "end_date", "is_completed"]
+        fields = ['title', 'description', 'start_date', 'end_date', 'is_completed', 'completed_image']
+        widgets = {
+            'description': SummernoteWidget(),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '제목을 입력해주세요.'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'completed_image': forms.FileInput(attrs={'class': 'form-control'})
+        }
+...
